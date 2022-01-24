e4d94c1239423adcb9175643a6deb592d302d5ab
linst <2205191664@qq.com>
2022-01-25 00:51:00 +0800

    init

---
+++ src/components/Hi.vue
@@ -0,0 +1,24 @@
+<template>
+<!--  <div class="hello">
+    <h1>{{msg}}</h1>
+  </div>-->
+  <div>
+    <router-link to="/">首页</router-link>|
+    <router-link to="/hi">Hi页面</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hi",
+  data() {
+    return{
+      msg: "Hi, i am linst"
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
